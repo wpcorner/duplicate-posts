c04ce6accc1a5ac2c3911ba2d0b5fbf11cda5c2e
Patrick Lumumba <97313939+lumumbapl@users.noreply.github.com>
2024-03-02 23:34:00 +0300

    Update duplicate-posts.php

--- duplicate-posts.php
+++ duplicate-posts.php
@@ -6,6 +6,7 @@
  * Version:           1.0.0
  * Author:            WP Corner
  * Author URI:        https://wpcorner.co/
+ * Contributors: wpcorner, lumiblog
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-duplicate-posts
